ebfd761ae93bc2d6e6635429ceec6d0314c82e50
Lucille Gillard <gillard.lucille@gmail.com>
2025-02-19 10:54:36 +0100

    Init SpringBoot project

---
+++ src/main/java/multiplayer_CYOA/MultiplayerCyoaBackendApplication.java
@@ -0,0 +1,13 @@
+package multiplayer_CYOA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerCyoaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplayerCyoaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/multiplayer_CYOA/MultiplayerCyoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package multiplayer_CYOA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerCyoaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
